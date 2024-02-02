62f5802b50ea09157fcaf622c43c134c5a5309c5
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-02 13:12:12 -0300

    se creo el componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import './App.css'
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Titulo from './components/Titulo';
+import Titulo from "./components/Titulo";
+import Container from "react-bootstrap/Container";
+import Footer from "./components/Footer";
 
 function App() {
-  
-
   return (
     <>
-    <Titulo></Titulo>
-      
+      <Titulo></Titulo>
+      <Container className="mainPage"></Container>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,7 +1,9 @@
 const Titulo = () => {
-    return (
-        <h1 className="text-center text-bg-dark py-3 display-3">Consulta el Clima</h1>
-    );
+  return (
+    <h1 className="text-center text-bg-dark py-3 display-3">
+      Consulta el Clima
+    </h1>
+  );
 };
 
 export default Titulo;
